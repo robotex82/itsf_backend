b19c888530ef36adb83a70cf29712f552c0176a2
Roberto Vasquez Angel <roberto@vasquez-angel.de>
2015-12-04 22:10:26 +0100

    Added module dashboards.

--- app/controllers/itsf/backend/home_controller.rb
+++ app/controllers/itsf/backend/home_controller.rb
@@ -2,6 +2,16 @@ module Itsf::Backend
   class HomeController < ApplicationController
     layout 'itsf/backend/base'
 
+    helper_method :engine
+
+    def self.engine
+      "#{name.deconstantize}::Engine".constantize
+    end
+
+    def engine
+      self.class.engine
+    end
+
     def index
     end
   end

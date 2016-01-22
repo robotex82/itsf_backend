692f3ce0601c40f72a87a9719778522e94b4224f
Roberto Vasquez Angel <roberto@vasquez-angel.de>
2016-01-22 17:17:19 +0100

    Added english translations Added assets

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,2 +1,3 @@
 Itsf::Backend::Engine.config.assets.precompile += %w( itsf_backend.css )
 Itsf::Backend::Engine.config.assets.precompile += %w( itsf_backend.js )
+Itsf::Backend::Engine.config.assets.precompile += %w( itsf/backend/favicon.png )

e3c2d8b8ce3110d3b9fbe0720d56ba80fce3bad9
Roberto Vasquez Angel <roberto@vasquez-angel.de>
2016-02-29 17:43:17 +0100

    Fixed error when resource don't have a index or new action.

--- app/forms/concerns/form/ui_autocomplete.rb
+++ app/forms/concerns/form/ui_autocomplete.rb
@@ -6,7 +6,7 @@ module Concerns
       def autocomplete(attribute, options = {})
         options.reverse_merge!(url: -> { url_for(action: attribute, format: :json) })
         url = options.delete(:url)
-        input attribute, input_html: { class: 'ui-autocomplete-input', :'data-source-url' => url.call }
+        input attribute, as: :string, input_html: { class: 'ui-autocomplete-input', :'data-source-url' => url.call }
       end
     end
   end

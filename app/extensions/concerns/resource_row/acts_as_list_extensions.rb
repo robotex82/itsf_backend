79b0af40d0c1b495ee4e9e4cc4abad6870f6b6b3
Roberto Vasquez Angel <roberto@vasquez-angel.de>
2016-10-08 02:23:01 +0200

    beautified

--- app/extensions/concerns/resource_row/acts_as_list_extensions.rb
+++ app/extensions/concerns/resource_row/acts_as_list_extensions.rb
@@ -12,9 +12,9 @@ module ResourceRow
       scope = "#{scope}_id".intern if scope.is_a?(Symbol) && scope.to_s !~ /_id$/
 
       data_attributes = {
-        'acts-as-list-item': true,
-        'acts-as-list-item-scope': "#{scope}-#{resource.send(scope)}",
-        'acts-as-list-item-uid': resource.to_param,
+        'acts-as-list-item':                true,
+        'acts-as-list-item-scope':          "#{scope}-#{resource.send(scope)}",
+        'acts-as-list-item-uid':            resource.to_param,
         'acts-as-list-item-on-drop-target': h.url_for([:reposition, resource])
       }
 

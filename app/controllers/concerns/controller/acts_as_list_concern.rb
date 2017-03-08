d3723f94a5858610cdb8e1f0ba6bf51776645512
Roberto Vasquez Angel <roberto@vasquez-angel.de>
2017-03-09 00:45:31 +0100

    Fix acts_as_list load_scope.

--- app/controllers/concerns/controller/acts_as_list_concern.rb
+++ app/controllers/concerns/controller/acts_as_list_concern.rb
@@ -4,7 +4,7 @@ module Controller
 
     def reposition
       @resource = load_resource
-      @dropped_resource = resource_class.find(params[:dropped_id])
+      @dropped_resource = load_scope.find(params[:dropped_id])
       @dropped_resource.set_list_position(@resource.position)
       position = @dropped_resource.position < @resource.position ? :before : :after
       redirect_to collection_path, notice: I18n.t("acts_as_list.inserted_#{position}", target_resource: @resource.try_all(*Itsf::Backend::Configuration.resource_title_methods), inserted_resource: @dropped_resource.try_all(*Itsf::Backend::Configuration.resource_title_methods))

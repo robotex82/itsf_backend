31b9d4377cef5ff19254e7edb3e652a0d3cf2037
Roberto Vasquez Angel <roberto@vasquez-angel.de>
2017-06-11 15:29:55 +0200

    Use redirect_back for rails > 5.

--- app/controllers/concerns/controller/acts_as_published_concern.rb
+++ app/controllers/concerns/controller/acts_as_published_concern.rb
@@ -5,9 +5,15 @@ module Controller
     def toggle_published
       @resource = load_resource
       @resource.toggle_published!
+
       action_taken = @resource.published? ? 'published' : 'unpublished'
       resource_label = @resource.try_all(*Itsf::Backend::Configuration.resource_title_methods)
-      redirect_to :back, notice: I18n.t("acts_as_published.notices.#{action_taken}", name: resource_label)
+      if Rails.version < '5.0.0'
+        redirect_to :back, notice: I18n.t("acts_as_published.notices.#{action_taken}", name: resource_label)
+      else
+        flash[:notice] = I18n.t("acts_as_published.notices.#{action_taken}", name: resource_label)
+        redirect_back(fallback_location: main_app.root_path)
+      end
     end
   end
 end

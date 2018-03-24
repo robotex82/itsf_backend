88670825be895030026a401094e4ca987ae947cb
Roberto Vasquez Angel <roberto@vasquez-angel.de>
2018-03-24 18:57:48 +0100

    Change service controller to use create and new instead of index, invoke and call methods to fit Rails::AddOns::Service::Base. Improve breadcrumbs for services (still not perfect).

--- app/controllers/concerns/controller/breadcrumbs_concern.rb
+++ app/controllers/concerns/controller/breadcrumbs_concern.rb
@@ -17,7 +17,7 @@ module Controller
         if respond_to?(:current_engine, true) && current_engine.present?
           breadcrumbs << { label: t("classes.#{current_engine.name.underscore}"), url: send("#{current_engine.engine_name}_#{I18n.locale}").root_path, link_html_options: {}, li_html_options: {} }
         end
-        
+
         if respond_to?(:resource_class, true)
           breadcrumbs << { label: resource_class.model_name.human(count: :other), url: url_for(action: :index), link_html_options: {}, li_html_options: {} }
         end
@@ -25,19 +25,23 @@ module Controller
         if @resource.present? && @resource.persisted?
           breadcrumbs << { label: @resource.try_all(*Itsf::Backend.resource_title_methods), url: url_for(action: :show, id: @resource.to_param), link_html_options: {}, li_html_options: {} }
         end
-        
+
         if respond_to?(:service_class, true)
-          breadcrumbs << { label: t("classes.#{service_class.name.underscore}"), url: url_for(action: :index), link_html_options: {}, li_html_options: {} }
+          breadcrumbs << t("classes.#{service_class.name.underscore}")
         end
 
-        if %w(new create edit update invoke call).include?(action_name)
+        if %w(new edit).include?(action_name)
+          breadcrumbs << { label: t("controller.breadcrumbs_concern.#{action_name}"), url: { action: action_name }, link_html_options: {}, li_html_options: {} }
+        end
+
+        if %w(create update).include?(action_name)
           breadcrumbs << { label: t("controller.breadcrumbs_concern.#{action_name}"), url: '#', link_html_options: {}, li_html_options: {} }
         end
-        
+
         if params[:page].present?
           breadcrumbs << { label: t('controller.breadcrumbs_concern.page', page_number: params[:page]), url: '#', link_html_options: {}, li_html_options: {} }
         end
-        
+
         breadcrumbs.last[:li_html_options][:class] = 'active'
       end
     end

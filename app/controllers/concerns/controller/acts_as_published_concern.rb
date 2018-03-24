547d68140755121e20b31429d539925ac70901fd
Roberto Vasquez Angel <roberto@vasquez-angel.de>
2018-03-24 15:13:48 +0100

    Fix translation paths.

--- app/controllers/concerns/controller/acts_as_published_concern.rb
+++ app/controllers/concerns/controller/acts_as_published_concern.rb
@@ -9,9 +9,9 @@ module Controller
       action_taken = @resource.published? ? 'published' : 'unpublished'
       resource_label = @resource.try_all(*Itsf::Backend::Configuration.resource_title_methods)
       if Rails.version < '5.0.0'
-        redirect_to :back, notice: I18n.t("acts_as_published.notices.#{action_taken}", name: resource_label)
+        redirect_to :back, notice: I18n.t("acts_as_published.flash.actions.toggle_published.notices.#{action_taken}", name: resource_label)
       else
-        flash[:notice] = I18n.t("acts_as_published.notices.#{action_taken}", name: resource_label)
+        flash[:notice] = I18n.t("acts_as_published.flash.actions.toggle_published.notices.#{action_taken}", name: resource_label)
         redirect_back(fallback_location: main_app.root_path)
       end
     end
